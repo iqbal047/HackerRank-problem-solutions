014ade5ad38e117364e2db70732347aebe0656d6
Md Iqbal Hossain <mdiqbalhossain20775@gmail.com>
2024-03-10 23:33:51 +0600

    Solve

---
+++ problem-solve/src/Solution.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int result = n * i;
+            System.out.println(n + " x " + i + " = " + result);
+        }
+
+        scanner.close();
+    }
+}
